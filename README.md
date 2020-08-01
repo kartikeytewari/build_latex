3f08f03c4bdae197d6e1d8b90088262c52dea913
Kartikey Tewari <kartikeya30@gmail.com>
2020-08-01 16:23:39 +0530

    added -i flag

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # build_latex
 Automating latex.
+
+sudo apt install apt-file  ->intalls apt-file
+sudo apt-file update       ->updates cache
--- main.sh
+++ main.sh
@@ -6,41 +6,40 @@ function build_latex()
 			# for building latex without bibliography
 			-bs|--build_small)
 			pdflatex ${1}.tex
+			shift
 			;;
+
 			# for building latex with bibliography
 			-bl|--build_large)
 			pdflatex ${1}.tex
 			bibtex ${1}.aux
 			pdflatex ${1}.tex
 			pdflatex ${1}.tex
+			shift
 			;;
-			 # for opening in google chrome
+			
+			# for opening in google chrome
 			-o|--open)
 			chrome ${1}.pdf
 			shift
 			;;
+	
+			# for finding which package to install for missing .sty file
+			-f|--find)
+			sudo apt-file find ${2}
+			shift
+			;;
+
+			# installs the .sty package container
+			-i|--install)
+			sudo apt-get install ${2}
+			shift
+			;;
+
 			# for printing manual
 			-h|--help) 
-			echo "build_latex function is used for building latex files"
-			echo "along with bibliography (if used)"
-			echo "flags:"
-			echo ""
-			echo "-bs | --build_small"
-			echo "		just builds the latex file"
-			echo ""
-			echo "-bl | --build_large"
-			echo "		builds the latex file along with bibliography"
-			echo "		if .bib file is not found then it is ignored"
-			echo ""
-			echo "-o | --open"
-			echo "		This opens the built pdf in chrome"
-			echo ""
-			echo "-h | --help"
-			echo "		Prints this manual page"
-			echo ""
-			echo "Under development. MIT LIcense 2020"
-			echo "For more information see www.kartikeytewari.github.io/blog/build_latex"
-			echo "for feedback/suggestions contact developer at: kartikeya30@gmail.com"
+			less manual.txt
+			shift
 		esac
 	done
 }
