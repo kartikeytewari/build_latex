55f240b9f451393174af417fd310b4d226569952
Kartikey Tewari <kartikeya30@gmail.com>
2020-08-03 10:56:06 +0530

    added populate flags

--- main.sh
+++ main.sh
@@ -24,6 +24,15 @@ function build_latex()
 			shift
 			;;
 	
+			# removes random files after buildup
+			-c|--clean)
+			rm ${1}.log
+			rm ${1}.aux
+			rm ${1}.out
+			rm ${1}.blg
+			shift
+			;;
+
 			# for finding which package to install for missing .sty file
 			-f|--find)
 			sudo apt-file find ${2}
@@ -40,6 +49,45 @@ function build_latex()
 			-h|--help) 
 			less ~/build_latex/manual.txt
 			shift
+
+
+			# ----------------------------------------------------------------------
+			# populating files with template
+			# article template
+			-p_article|--populate_article)
+			touch ${1}.tex
+			~/build_latex/template/article.tex > ${1}.tex
+			shift
+			;;
+
+			# beamer template
+			-p_beamer|--populate_beamer)
+			touch ${1}.tex
+			~/build_latex/template/beamer.tex > ${1}.tex
+			shift
+			;;
+
+			# letter template
+			-p_letter|--populate_letter)
+			touch ${1}.tex
+			~/build_latex/template/letter.tex > ${1}.tex
+			shift
+			;;
+
+			# cv template
+			-p_cv|--populate_cv)
+			touch ${1}.tex
+			~/build_latex/template/cv.tex > ${1}.tex
+			shift
+			;;
+
+			# report template
+			-p_report|--populate_report)
+			touch ${1}.tex
+			~/build_latex/template/report.tex > ${1}.tex
+			shift
+			;;
+			# ----------------------------------------------------------------------
 		esac
 	done
 }
